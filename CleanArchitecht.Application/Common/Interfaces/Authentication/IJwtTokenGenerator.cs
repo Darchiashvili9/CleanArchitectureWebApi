6bb7868e8306af2d1b1ef2c81529095466573a06
axalisamsa1827 <gdarchiashvili@hotmail.com>
2023-04-28 20:53:02 +0400

    JwtToken generator

---
+++ CleanArchitecht.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecht.Application.Common.Interfaces.Authentication
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToke(Guid userId, string firstName, string lastName);
+
+    }
+}
--- CleanArchitecht.Application/Services/Authentication/AuthenticationService.cs
+++ CleanArchitecht.Application/Services/Authentication/AuthenticationService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchitecht.Application.Common.Interfaces.Authentication;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,21 @@ namespace CleanArchitecht.Application.Services.Authentication
 {
     public class AuthenticationService : IAuthenticationService
     {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
         public AuthenticationResult Register(string firstName, string lastName, string email, string password)
         {
+            //check if user already exists
+
+            //create user
+
+            //create jwt token
+
             return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, "token");
         }
 
